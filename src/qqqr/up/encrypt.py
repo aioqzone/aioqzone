e8c8ec62e12ae6dcc85456e27a60ddbf89ffff3a
JamzumSum <zzzzss990315@gmail.com>
2022-04-11 10:57:49 +0800

    fix: TeaEncoder

--- src/qqqr/up/encrypt.py
+++ src/qqqr/up/encrypt.py
@@ -7,7 +7,7 @@ import re
 import struct
 from abc import ABC, abstractmethod
 from random import randint
-from typing import Awaitable
+from typing import Awaitable, Union
 
 import rsa
 from aiohttp import ClientSession
@@ -37,12 +37,8 @@ class PasswdEncoder(ABC):
 
 
 class NodeEncoder(PasswdEncoder):
-    """
-    AGPL-3.0
-
-    Copyright (C) 2021-2022 aioqzone
-
-    .. seealso:: https://github.com/JamzumSum/QQQR/blob/master/LICENCE
+    """Encoder using original js code by communicating with local :program:`Node.js <node>` progress.
+    Make sure this always work.
     """
 
     __f = None
@@ -67,14 +63,19 @@ class NodeEncoder(PasswdEncoder):
 
 
 class TeaEncoder(PasswdEncoder):
-    """The MIT License
+    """Pure python password encoder implementation using tea and rsa. (beta)
 
-    Copyright (c) 2005 hoxide
+    .. code-block::txt
+        :caption: LICENSE
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-    QQ Crypt module.
+        The MIT License
+
+        Copyright (c) 2005 hoxide
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+        The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+        QQ Crypt module.
 
     .. seealso:: https://github.com/LeoHuang2015/qqloginjs/blob/7d82f2f7d7363547763c40ce5d258d18989b9732/tea.py"""
 
@@ -91,15 +92,15 @@ class TeaEncoder(PasswdEncoder):
         return r
 
     def _tea(self, v: bytes, t: bytes):
-        n = 16  # qq use 16
-        k = struct.unpack(">LLLL", t[0:16])
+        o, r, a, l = struct.unpack(">LLLL", t[0:16])
         y, z = struct.unpack(">LL", v[0:8])
         s = 0
-        for i in range(n):
+        for i in range(16):
             s += self.delta
-            y += (self.op & (z << 4)) + k[0] ^ z + s ^ (self.op & (z >> 5)) + k[1]
+            s &= self.op
+            y += (z << 4) + o ^ z + s ^ (z >> 5) + r
             y &= self.op
-            z += (self.op & (y << 4)) + k[2] ^ y + s ^ (self.op & (y >> 5)) + k[3]
+            z += (y << 4) + a ^ y + s ^ (y >> 5) + l
             z &= self.op
         r = struct.pack(">LL", y, z)
         return r
@@ -111,12 +112,11 @@ class TeaEncoder(PasswdEncoder):
         vl = len(v)
         filln = (8 - (vl + 2)) % 8 + 2
         fills = bytes(randint(0, 0xFF) for _ in range(filln))
-        v = bytes([(filln - 2) | FILL_N_OR]) + fills + v + END_CHAR * 7
-        tr = b"\0" * 8
-        to = b"\0" * 8
+        v = bytes([randint(0, FILL_N_OR) | (filln - 2)]) + fills + v + END_CHAR * 7
+        assert len(v) % 8 == 0
+
+        tr = to = o = b"\0" * 8
         r = b""
-        o = b"\0" * 8
-        # print 'len(v)=', len(v)
         for i in range(0, len(v), 8):
             o = self._xor(v[i : i + 8], tr)
             tr = self._xor(self._tea(o, k), to)
@@ -124,36 +124,48 @@ class TeaEncoder(PasswdEncoder):
             r += tr
         return r
 
-    def _tx_md5(self, raw_str: bytes):
+    def _tx_md5(self, raw_str: Union[bytes, str]):
+        if isinstance(raw_str, str):
+            raw_str = raw_str.encode()
         return hashlib.md5(raw_str).hexdigest().upper()
 
-    def _hex2str(self, ct: int):
+    def _int2hex(self, ct: int):
         return hex(ct)[2:]
 
     def _fromhex(self, s: str):
         return bytes(bytearray.fromhex(s))
 
-    async def getEncryption(self, salt: str, verifycode: str):
-        e = salt.encode()
-        # md5_pwd = o = self.tx_md5(pwd.encode())
-        r = hashlib.md5(self._passwd.encode()).digest()
-        p = self._tx_md5(r + e)
-        a = rsa.encrypt(r, self.rsaKey)
-        rsaData = binascii.b2a_hex(a)
+    def _hex2bytes(self, s: bytes):
+        e = []
+        for i in range(0, len(s), 2):
+            try:
+                e.append(int(s[i : i + 2]))
+                continue
+            except ValueError:
+                pass
+            try:
+                e.append(int(s[i]))
+            except ValueError:
+                e.append(0)
+        return bytes(e)
 
-        # rsa length
-        s = self._hex2str(int(len(rsaData) / 2))
-        s = s.zfill(4)
-
+    async def getEncryption(self, salt: str, verifycode: str, md5_pwd: bool = False):
         # verifycode先转换为大写，然后转换为bytes
         l = binascii.b2a_hex(verifycode.upper().encode())
 
         # verifycode length
-        c = self._hex2str(int(len(l) / 2))
+        c = self._int2hex(int(len(l) / 2)).encode()
         c = c.zfill(4)
 
-        # TEA: KEY:p, s+a+ TEA.strToBytes(e) + c +l
-        # aioqzone EDIT: I tried `binascii.b2a_hex(e)` to replace `salt` to fix error
-        new_pwd = s.encode() + rsaData + binascii.b2a_hex(e) + c.encode() + l
-        enc = self.encrypt(self._fromhex(new_pwd.decode()), self._fromhex(p))
-        return base64.b64encode(enc, b"*-").decode().replace("=", "_")
+        r = self._passwd
+        if md5_pwd:
+            r = self._tx_md5(self._passwd)
+        r = r.encode()
+
+        p = self._tx_md5(self._hex2bytes(r) + salt.encode())
+        n = self.encrypt(r + binascii.b2a_hex(salt.encode()) + c + l, p.encode())
+
+        r = self._int2hex(int(len(n) / 2)).encode()
+        n = rsa.encrypt(self._hex2bytes(r.zfill(4) + n), self.rsaKey)
+
+        return base64.b64encode(self._hex2bytes(n), b"*-").decode().replace("=", "_")
