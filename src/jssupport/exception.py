bd99b971c368ea7902eb40ef8bdb5f3ade5fe699
JamzumSum <zzzzss990315@gmail.com>
2022-04-16 11:00:14 +0800

    FATAL: execjs api change
    refactor: execjs
    feat: add Js Partial
    feat: add execjs exceptions
    feat: add setup and post statement in execjs
    test: update
    fix: remove window.js (update #31)

---
+++ src/jssupport/exception.py
@@ -0,0 +1,20 @@
+from subprocess import CalledProcessError
+
+
+class JsRuntimeError(CalledProcessError):
+    """Error in executing javascript."""
+
+    def __init__(self, returncode: int, cmd: str, stderr: bytes) -> None:
+        super().__init__(returncode, cmd=cmd, stderr=stderr)
+
+
+class NodeNotFoundError(FileNotFoundError):
+    """Node executable not found."""
+
+    pass
+
+
+class JsImportError(RuntimeError):
+    """Js module not installed."""
+
+    pass
--- src/jssupport/jsdom.py
+++ src/jssupport/jsdom.py
@@ -1,33 +1,55 @@
-from pathlib import Path
 from shutil import which
+from textwrap import dedent
 
-from jssupport.execjs import ExecJS
+from .exception import JsImportError
+from .execjs import ExecJS
 
 
 class JSDOM(ExecJS):
     """.. note:: requires :js:mod:`jsdom` installed."""
 
-    def __init__(self, node: str = "node", *, src: str, ua: str, location: str, referrer: str):
-        src = src.replace("\n", " ")
-        pre_def = f"var src=`{src}`,ua='{ua}',location='{location}',referrer='{referrer}';\n"
-        super().__init__(node, js=pre_def + self._windowjs())
-        assert self.check_jsdom(), "jsdom should be installed."
-
-    def _windowjs(self):
-        """Override this if you have other js files."""
-        with open(Path(__file__).parent / "window.js") as f:
-            return f.read()
-
-    def eval(self, script: str):
-        return self("window.eval", script)
-
-    def add_eval(self, script: str):
-        self.addfunc("window.eval", script)
-
-    def check_jsdom(self):
+    @classmethod
+    def check_jsdom(cls):
         from subprocess import run
 
         require = lambda m: run(
-            self.node, executable=which(self.node), input=f"require('{m}')", text=True
+            cls.node, executable=which(cls.node), input=f"require('{m}')", text=True
         )
         return require("jsdom").returncode == 0
+
+    def check_all(self):
+        super().check_all()
+        if not self.check_jsdom():
+            raise JsImportError("jsdom")
+
+    def __init__(self, *, src: str = "", ua: str = "", location: str = "", referrer: str = ""):
+        super().__init__()
+        src = src.replace("\n", " ")
+        pre_def = f"var src=`{src}`,ua='{ua}',location='{location}',referrer='{referrer}';\n"
+        self.setup.append(pre_def)
+        self.setup.append(self._windowjs())
+
+    def _windowjs(self):
+        """Override this if you have other js files."""
+
+        js = """
+        const jsdom = require("jsdom");
+        const { JSDOM } = jsdom;
+        const dom = new JSDOM(src, {
+            pretendToBeVisual: true,
+            url: location,
+            referrer: referrer,
+            userAgent: ua,
+            pretendToBeVisual: true,
+            runScripts: "outside-only",
+        });
+        dom.reconfigure({ windowTop: {} });
+        window = dom.window;
+        """
+        return dedent(js)
+
+    def eval(self, script: str):
+        return self(f"window.eval({script})")
+
+    def add_eval(self, script: str):
+        self.add_run("window.eval", script)
