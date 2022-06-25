3130ab9e1283a24a5e1c3118300b8f3576ebd05a
JamzumSum <JamzumSum@users.noreply.github.com>
2022-06-25 03:01:03 +0000

    deploy: 1afd1a7f6fdc63d3836173654695c43c045d4ef9

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.0a1.dev8',
+    VERSION: '0.9.2a2.dev1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
