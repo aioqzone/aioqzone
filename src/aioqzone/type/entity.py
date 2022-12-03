05c1b8ba8417b8a546e75fe3fde8a88f7b91c284
JamzumSum <zzzzss990315@gmail.com>
2022-12-03 12:29:20 +0800

    feat: add __eq__ for entities

--- src/aioqzone/type/entity.py
+++ src/aioqzone/type/entity.py
@@ -14,32 +14,47 @@ class HasContent(BaseModel):
 
 
 class ConEntity(BaseModel):
-    """Base class for all content entities. All entities has a #type."""
+    """Base class for all content entities. All entities has a numeric :obj:`type`."""
 
     type: int
     """Official qzone type is positive. Our self-defined entities will have a negative type."""
 
 
 class TextEntity(ConEntity):
-    """Text, type=2."""
+    """Text, type=2.
+
+    Two objects of this class are considered equal, if their :obj:`con` is equal."""
 
     type: int = 2
     con: str = ""
 
+    def __eq__(self, other) -> bool:
+        return isinstance(other, TextEntity) and self.con == other.con
+
 
 class AtEntity(ConEntity):
-    """At, type=0. like: ``@bot``"""
+    """At, type=0. like: ``@bot``
+
+    Two objects of this class are considered equal, if their :obj:`uin` is equal."""
 
     type: int = 0
     nick: str = ""
     uin: int
 
+    def __eq__(self, other) -> bool:
+        return isinstance(other, AtEntity) and self.uin == other.uin
+
 
 class EmEntity(ConEntity):
-    """Emoji, type=-1. like: ``[em]e100[/em]``"""
+    """Emoji, type=-1. like: ``[em]e100[/em]``
+
+    Two objects of this class are considered equal, if their :obj:`eid` is equal."""
 
     type: int = -1
     eid: int
+
+    def __eq__(self, other):
+        return isinstance(other, EmEntity) and self.eid == other.eid
 
 
 RespEntities = Union[AtEntity, TextEntity]
