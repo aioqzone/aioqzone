Fix int overflow in `corr_norm_mask_match ` (#124)
* feat: add api to switch to h5 proxy

* fix!: `corr_norm_mask_match` int overflow

* feat(alg): little improvement in matching

---------

Co-authored-by: JamzumSum <zzzzss990315@gmail.com>

src/qqqr/up/captcha/jigsaw.py
@@ -9,9 +9,10 @@ from PIL import Image as image
 
 if TYPE_CHECKING:
     mat_u1 = np.ndarray[Any, np.dtype[np.uint8]]
+    mat_i2 = np.ndarray[Any, np.dtype[np.int16]]
     mat_i4 = np.ndarray[Any, np.dtype[np.int32]]
 else:
-    mat_u1 = mat_i4 = np.ndarray
+    mat_u1 = mat_i2 = mat_i4 = np.ndarray
 
 debug = bool(env.get("AIOQZONE_JIGSAW_DEBUG"))
 
@@ -37,8 +38,12 @@ def conv2d(x: np.ndarray, k: np.ndarray, axes=(0, 1)):
     return back.real[kh - 1 :, kw - 1 :]
 
 
-def corr_norm_mask_match(x: mat_u1, tmpl: mat_u1, mask: mat_u1):
+def corr_norm_mask_match(x: mat_i2, tmpl: mat_i2, mask: mat_u1):
     r"""``TM_CCORR_NORMED`` mode in opecv matchTemplate.
+
+    :param x: data mat. dtype should be int/uint.
+    :param tmpl: template mat. dtype should be the same with that of `x`.
+    :param mask: a binary mask mat in ``{0, 1}``. dtype should be int/uint.
 
     .. math::
 
@@ -48,7 +53,14 @@ def corr_norm_mask_match(x: mat_u1, tmpl: mat_u1, mask: mat_u1):
     mask_template = np.flip(mask_template, axis=(0, 1))
     mask_template = mask_template / np.linalg.norm(mask_template, axis=(0, 1))
     corr = conv2d(x, mask_template)
-    img_norm = np.sqrt(conv2d(np.power(x, 2), mask))
+
+    # NOTE: suppose x is in (-(2^8 - 1), 2^8 - 1), saved in int16
+    # then x^2 is in (0, 2^16-1), cannot be saved in int16 but can be saved in uint16.
+    # so we should cast |x| into uint16 and calc. |x|^2, which is in uint16.
+    abs_x = np.abs(x).astype(np.dtype(f"u{x.dtype.itemsize}"))
+
+    mask = np.flip(mask, axis=(0, 1))
+    img_norm = np.sqrt(conv2d(np.power(abs_x, 2), mask))
     img_norm[img_norm < 1e-12] = +np.inf
     return (corr / img_norm).sum(axis=-1)
 
@@ -56,8 +68,8 @@ def corr_norm_mask_match(x: mat_u1, tmpl: mat_u1, mask: mat_u1):
 def hdiff_corr_norm_mask_match(x: mat_u1, tmpl: mat_u1, mask: mat_u1):
     x = x.astype(np.int16)
     tmpl = tmpl.astype(np.int16)
-    x_diff = x[:, 1:] - x[:, :-1]
-    t_diff = tmpl[:, 1:] - tmpl[:, :-1]
+    x_diff: mat_i2 = x[:, 1:] - x[:, :-1]  # type: ignore
+    t_diff: mat_i2 = tmpl[:, 1:] - tmpl[:, :-1]  # type: ignore
     mask_u = mask[:, 1:] * mask[:, :-1]
     return corr_norm_mask_match(x_diff, t_diff, mask_u)
 
@@ -65,10 +77,32 @@ def hdiff_corr_norm_mask_match(x: mat_u1, tmpl: mat_u1, mask: mat_u1):
 def vdiff_corr_norm_mask_match(x: mat_u1, tmpl: mat_u1, mask: mat_u1):
     x = x.astype(np.int16)
     tmpl = tmpl.astype(np.int16)
-    y_diff = x[1:] - x[:-1]
-    t_diff = tmpl[1:] - tmpl[:-1]
+    y_diff: mat_i2 = x[1:] - x[:-1]  # type: ignore
+    t_diff: mat_i2 = tmpl[1:] - tmpl[:-1]  # type: ignore
     mask_u = mask[1:] * mask[:-1]
     return corr_norm_mask_match(y_diff, t_diff, mask_u)
+
+
+def energy_mask(x: mat_u1, tmpl: mat_u1, mask: mat_u1, threshold=0.6):
+    mask_template = mask * tmpl
+    x_u2 = x.astype(np.uint16)
+    tpl_energy = np.linalg.norm(mask_template, axis=(0, 1))
+
+    mask = np.flip(mask, axis=(0, 1))
+    img_sqe = np.sqrt(conv2d(np.power(x_u2, 2), mask))
+    return np.any(img_sqe > (threshold * tpl_energy), axis=-1)
+
+
+def sdiff_mask_match(x: mat_u1, tmpl: mat_u1, mask: mat_u1):
+    mask_template = mask * tmpl
+    mask_template = np.flip(mask_template, axis=(0, 1))
+    corr = conv2d(x, mask_template)
+
+    x_u2 = x.astype(np.uint16)
+    mask = np.flip(mask, axis=(0, 1))
+    img_energy = conv2d(np.power(x_u2, 2), mask)
+
+    return (img_energy - 2 * corr).sum(axis=-1)
 
 
 class Piece:
@@ -146,11 +180,11 @@ class Piece:
         ys, xs = self._yx_range
         return self.mask[ys, xs]
 
-    def build_template(self, a: float = 0.3) -> mat_u1:
+    def build_template(self, a: float = 0.38) -> mat_u1:
         """This method attempts to generate a piece view like that on the puzzle by dimming the original piece.
         In order to get an accurate result.
 
-        :param a: coeff to be multiplied with the image in order to dim it, default as 0.3
+        :param a: coeff to be multiplied with the image in order to dim it, default as 0.36
         :return: generated piece image.
         """
         return (self.strip() * a).round().astype(np.uint8)
@@ -242,8 +276,12 @@ class Jigsaw:
             h_cfd = hdiff_corr_norm_mask_match(x, template, mask)
             v_cfd = vdiff_corr_norm_mask_match(x, template, mask)
             self.confidence = h_cfd + v_cfd
+            filt = energy_mask(x, self.piece.strip(), mask)
+            if not np.all(filt):
+                self.confidence[filt] = 0
 
             if debug:
+                sdiff = sdiff_mask_match(x, template, mask)
                 debug_out = Path("data/debug")
                 debug_out.mkdir(exist_ok=True, parents=True)
 
@@ -261,6 +299,7 @@ class Jigsaw:
                         cfd_bands(h_cfd),
                         cfd_bands(v_cfd),
                         cfd_bands(self.confidence),
+                        cfd_bands(-sdiff),
                     ],
                     axis=0,
                 )