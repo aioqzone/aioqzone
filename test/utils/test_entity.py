05b04e6ae168843877a664f9bc7db3bdf65e24f0
JamzumSum <zzzzss990315@gmail.com>
2023-04-22 10:54:44 +0800

    feat: add `LinkEntity` parsing rules and test

--- test/utils/test_entity.py
+++ test/utils/test_entity.py
@@ -3,7 +3,7 @@ from aioqzone.utils.entity import finfo_box_entities, split_entities
 
 
 def test_text_entity():
-    txt = """foo[em]e100[/em][em]e400399[/em]bar@{uin:12345,nick:spam}foobar[em]e400599[/em]end"""
+    txt = """foo[em]e100[/em][em]e400399[/em]bar@{uin:12345,nick:spam}foobar[em]e400599[/em]{url:https://example.com&a=1%20,text:网页链接}end"""
     entities = split_entities(txt)
     assert entities == [
         TextEntity(con="foo"),
@@ -13,6 +13,7 @@ def test_text_entity():
         AtEntity(uin=12345, nick="spam"),
         TextEntity(con="foobar"),
         EmEntity(eid=400599),
+        LinkEntity(url="https://example.com&a=1%20"),
         TextEntity(con="end"),
     ]
 
@@ -21,7 +22,7 @@ def test_finfo_entity():
     from lxml.html import fromstring
 
     html = """
-    foo<img src="http://qzonestyle.gtimg.cn/qzone/em/e100.png"><img src="http://qzonestyle.gtimg.cn/qzone/em/e400399.png">bar<a href="https://user.qzone.qq.com/12345">@spam</a>foobar<img src="http://qzonestyle.gtimg.cn/qzone/em/e400599.png">end"""
+    foo<img src="http://qzonestyle.gtimg.cn/qzone/em/e100.png"><img src="http://qzonestyle.gtimg.cn/qzone/em/e400399.png">bar<a href="https://user.qzone.qq.com/12345">@spam</a>foobar<img src="http://qzonestyle.gtimg.cn/qzone/em/e400599.png"><a href="https://example.com&a=1%20">网页链接</a>end"""
     entities = finfo_box_entities(fromstring(html))
     assert entities == [
         TextEntity(con="foo"),
@@ -31,5 +32,6 @@ def test_finfo_entity():
         AtEntity(uin=12345, nick="spam"),
         TextEntity(con="foobar"),
         EmEntity(eid=400599),
+        LinkEntity(url="https://example.com&a=1%20"),
         TextEntity(con="end"),
     ]
