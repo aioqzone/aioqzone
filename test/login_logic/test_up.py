d7f2b9c6aa45a87382bc4415b6602fae6923195a
JamzumSum <zzzzss990315@gmail.com>
2023-11-27 19:54:45 +0800

    test: fix wrong fixture in h5 login

--- test/login_logic/test_up.py
+++ test/login_logic/test_up.py
@@ -89,7 +89,7 @@ async def test_h5_login(h5: UpH5Login):
             if not h5.sms_code_input.has_impl:
                 pytest.skip(str(e))
         elif e.code == StatusCode.NeedCaptcha:
-            if not web.captcha.solve_select_captcha.has_impl:
+            if not h5.captcha.solve_select_captcha.has_impl:
                 pytest.skip("cannot solve captcha")
 
         raise
