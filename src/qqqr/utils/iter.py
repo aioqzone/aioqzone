4889253dfc32574a70ee69b44d2759ab77e3b086
JamzumSum <zzzzss990315@gmail.com>
2023-12-08 21:05:40 +0800

    style: minor naming style change

--- src/qqqr/utils/iter.py
+++ src/qqqr/utils/iter.py
@@ -1,29 +1,35 @@
-from typing import Callable, Iterable, Optional, TypeVar, Union, overload
+import typing as t
 
-T = TypeVar("T")
-D = TypeVar("D")
+T = t.TypeVar("T")
+D = t.TypeVar("D")
 
 
-@overload
-def first(it: Iterable[T], pred: Optional[Callable[[T], Union[object, None]]] = None) -> T:
+@t.overload
+def first(it: t.Iterable[T], pred: t.Optional[t.Callable[[T], t.Optional[object]]] = None) -> T:
     ...
 
 
-@overload
+@t.overload
 def first(
-    it: Iterable[T], pred: Optional[Callable[[T], Union[object, None]]] = None, *, default: D
-) -> Union[T, D]:
+    it: t.Iterable[T],
+    pred: t.Optional[t.Callable[[T], t.Optional[object]]] = None,
+    *,
+    default: D,
+) -> t.Union[T, D]:
     ...
 
 
 def first(
-    it: Iterable[T], pred: Optional[Callable[[T], Union[object, None]]] = None, *, default: D = ...
-) -> Union[T, D]:
+    it: t.Iterable[T],
+    pred: t.Optional[t.Callable[[T], t.Optional[object]]] = None,
+    *,
+    default: D = ...,
+) -> t.Union[T, D]:
     f = filter(pred, it)
     if default is ...:
         return next(f)
     return next(f, default)
 
 
-def firstn(it: Iterable[T], pred: Optional[Callable[[T], Union[object, None]]] = None):
+def firstn(it: t.Iterable[T], pred: t.Optional[t.Callable[[T], t.Optional[object]]] = None):
     return first(it, pred, default=None)
